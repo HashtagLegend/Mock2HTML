fb43dc47a47139c2230ac90676d00e86e1ee4872
Patrick Thomas Ørum <Patr421b@edu.easj.dk>
2018-11-01 20:44:51 +0100

    created mock HTML

--- src/js/index.ts
+++ src/js/index.ts
@@ -7,12 +7,24 @@ interface iCar{
   id: number;
 }
 
-const uri: string = "https://restservicet3st.azurewebsites.net/api/car/";
+const uri: string = "https://mock2service.azurewebsites.net/api/car/";
 
 let getAllCarsButton: HTMLButtonElement = document.getElementById("getAllCars") as HTMLButtonElement
 getAllCarsButton.addEventListener("click", getAllCars)
 
 let field1: HTMLParagraphElement = document.getElementById("field1") as HTMLParagraphElement
+let carByIdField: HTMLParagraphElement = document.getElementById("carByIdField") as HTMLParagraphElement
+
+let getCarByIdButton: HTMLButtonElement = document.getElementById("getCarById") as HTMLButtonElement
+getCarByIdButton.addEventListener("click", getCarById)
+
+let createCarButton: HTMLButtonElement = document.getElementById("createCar") as HTMLButtonElement
+createCarButton.addEventListener("click", createCar)
+
+let deleteCarButton: HTMLButtonElement = document.getElementById("deleteCar") as HTMLButtonElement
+deleteCarButton.addEventListener("click", deleteCar)
+
+
 
 
 
@@ -28,4 +40,44 @@ function getAllCars(){
         result += "</ul>";
         field1.innerHTML = result;
     })
+}
+
+function getCarById(): void {
+    let carId: HTMLInputElement = document.getElementById("carId") as HTMLInputElement
+    let newUri = uri + carId.value
+
+    axios.get<iCar>(newUri)
+       .then(function(response){
+        carByIdField.innerHTML = response.data.make + " " + response.data.type
+      })
+
+}
+
+function createCar(): void {
+    
+let myIdElm: HTMLInputElement = document.getElementById("id") as HTMLInputElement
+let myMakeElm: HTMLInputElement = document.getElementById("make") as HTMLInputElement
+let myTypeElm: HTMLInputElement = document.getElementById("type") as HTMLInputElement
+let myPriceElm: HTMLInputElement = document.getElementById("price") as HTMLInputElement
+let statusBar: HTMLParagraphElement = document.getElementById("statusBar") as HTMLParagraphElement
+
+let myId: number = +myIdElm.value
+let myMake: string = myMakeElm.value
+let myType: string = myTypeElm.value
+let myPrice: number = +myPriceElm.value
+
+axios.post<iCar>(uri, {id: myId, make: myMake, type: myType, price: myPrice})
+    .then((response: AxiosResponse) => {statusBar.innerHTML = (response.status + " " + response.statusText + " Bil tilføjet")})
+  
+  .catch(function(error: AxiosError):void {
+      statusBar.innerHTML = error.message;
+  })
+
+}
+function deleteCar(): void {
+    let statusBar: HTMLParagraphElement = document.getElementById("deleteStatusBar") as HTMLParagraphElement
+    let carId: HTMLInputElement = document.getElementById("deleteId") as HTMLInputElement
+    let newUri = uri + carId.value
+    axios.delete(newUri)
+    .then((response: AxiosResponse) => {statusBar.innerHTML = (response.status + " Car deleted")})
 }
